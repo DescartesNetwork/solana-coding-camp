19850c576436f46ec382e67e2e60e5bd1ca5c69f
tuanle161099 <tuanle@descartes.network>
2022-11-04 14:47:27 +0700

    update link speaker

--- src/view/home/mediaPartner/listPartners.tsx
+++ src/view/home/mediaPartner/listPartners.tsx
@@ -10,7 +10,7 @@ const MEDIA_PARTNER = [
   '1eutyoRworLkr3fm2ZvGw-T4ry-Z_cclY', // sequio
   '1DY1WG0j25oBcZ8c7NyFhNdcftY3Z3vQs', // Accel
   '1qnSJhrEnug8WhYfqpK3uKb0E_nh5RgtL', // LSVP
-  '1vC-7Y1m-NjnAFQdegvdU5D2S0Q0Z4x1p', //TK
+  '1LULYUJHYB1sjrrLA7uja6u1FOAMjuuxQ', //TK
   '1bUSAzz5PVTiHmUIBP3DEK-PC1LqkGGgG', //kyros
   '144MMNyGkI76HWFGUfcpygyGe77ocDTbC', //kyber
   '1j3h--AGs3fbk9efjy1O4kBKekRz6pZJe', // Krystal
